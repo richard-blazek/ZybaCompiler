c65d4ae36abbbf135a78df5934b2ef5f895f69dd
Richard Blažek <blazek.risa@gmail.com>
2021-07-08 12:04:03 +0200

    Parser, doesn't pass tests

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,24 +7,26 @@ data Expression = Integer Integer
                 | Rational Rational
                 | String String
                 | Variable String
-                | BinaryOperation Operator Expression Expression
+                | BinaryOperation Operator Expression Expression deriving (Show, Eq)
 
 parseValue :: [Token] -> (Expression, [Token])
 parseValue (LiteralInteger _ n : tokens) = (Integer n, tokens)
 parseValue (LiteralDecimal radix n exp : tokens) = (Rational (n % (radix ^ exp)), tokens)
 parseValue (LiteralString string : tokens) = (String string, tokens)
 parseValue (Identifier name : tokens) = (Variable name, tokens)
+parseValue (ParenthesisOpen : tokens) = case parseExpression tokens of
+    (expression, ParenthesisClose : restTokens) -> (expression, restTokens)
 
 parseBinaryOperation :: Expression -> [Token] -> (Expression, [Token])
-parseBinaryOperation current (Operator operator : tokens) = parseBinaryOperation operation newTokens
+parseBinaryOperation current (Operator operator : tokens) = parseBinaryOperation operation restTokens
     where
-        (operand, newTokens) = parseValue tokens
+        (operand, restTokens) = parseValue tokens
         operation = BinaryOperation operator current operand
 parseBinaryOperation current tokens = (current, tokens)
 
 parseExpression :: [Token] -> (Expression, [Token])
-parseExpression tokens = parseBinaryOperation value rest
-    where (value, rest) = parseValue tokens
+parseExpression tokens = parseBinaryOperation value restTokens
+    where (value, restTokens) = parseValue tokens
 
 parse :: [Token] -> Expression
 parse = fst . parseExpression
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,13 +9,13 @@ main = do
   _ <- runTestTT tests
   return ()
 
-tests = TestList [testLexer, testLexer2, testLexer3]
+tests = TestList [testLexer, testLexer2, testLexer3, testParser, testParser2]
 
 testLexer = TestCase (assertEqual "Tokenization of sample text"
-        (tokenize "1+2\n222\"abc+-/*!%74漢語if^\nelse1while\"*8+5.-4.44/hhh_hh")
+        (tokenize "1+2\n222\"abc+-/*!%74漢語if^\nelse1while\"*8+5.-4.404/hhh_hh")
         [LiteralInteger 10 1, Operator Plus, LiteralInteger 10 2, LiteralInteger 10 222, LiteralString "abc+-/*!%74漢語if\nelse1while",
         Operator Multiply, LiteralInteger 10 8, Operator Plus, LiteralDecimal 10 5 0, Operator Minus,
-        LiteralDecimal 10 444 2, Operator Divide, Identifier "hhh_hh"])
+        LiteralDecimal 10 4404 3, Operator Divide, Identifier "hhh_hh"])
 
 testLexer2 = TestCase (assertEqual "Tokenization of mathematical expressions"
         (tokenize "16rFF--11r5 0\n7//5->var    **4^5&7>=4<=2r0.11~7<-<<fu:16r10")
@@ -28,3 +28,10 @@ testLexer2 = TestCase (assertEqual "Tokenization of mathematical expressions"
 testLexer3 = TestCase (assertEqual "Tokenization of comments"
         (tokenize "{This is a comment{which shou\"ld be ignored}and it is}\"I said: ^\"Ho, ho^\" and {it} worked!\"{}7")
         [LiteralString "I said: \"Ho, ho\" and {it} worked!", LiteralInteger 10 7])
+
+testParser = TestCase (assertEqual "Parsing first mathematical expression" (parse (tokenize "((13))")) (Integer 13))
+
+testParser2 = TestCase (assertEqual "Parsing first mathematical expression"
+        (parse (tokenize "1/(1+1/(3.1416))"))
+        (BinaryOperation Divide (Integer 1) (BinaryOperation Divide (BinaryOperation Plus (Integer 1) (Integer 1))
+        (Rational (31416 % 10000)))))
