b002e46287f793045cec098e87b8f89404d2b21e
Richard Blažek <blazek.risa@gmail.com>
2021-07-12 21:59:27 +0200

    Changed rules for parentheses and calls

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,7 +7,7 @@ data Expression = Integer Integer
                 | Rational Rational
                 | String String
                 | Variable String
-                | Call Expression [Expression]
+                | Tuple [Expression]
                 | BinaryOperation Operator Expression Expression
                 | IfStatement [(Expression, [Expression])] (Maybe [Expression])
                 | WhileLoop (Expression, [Expression]) deriving (Show, Eq)
@@ -17,32 +17,32 @@ parseValue (LiteralInteger _ n : tokens) = (Integer n, tokens)
 parseValue (LiteralDecimal radix n exp : tokens) = (Rational (n % (radix ^ exp)), tokens)
 parseValue (LiteralString string : tokens) = (String string, tokens)
 parseValue (Identifier name : tokens) = (Variable name, tokens)
-parseValue (ParenthesisOpen : tokens) = case parseExpression tokens of 
-    (expression, ParenthesisClose : restTokens) -> (expression, restTokens)
+parseValue (ParenthesisOpen : tokens)
+    | isOne = (head expressions, restTokens)
+    | otherwise = (Tuple expressions, restTokens)
+    where
+        (expressions, _, restTokens) = parseBlock [ParenthesisClose] [] tokens
+        isOne = tail expressions == []
 parseValue (Keyword If : tokens) = parseIf [] tokens
 parseValue (Keyword While : tokens) = parseWhile tokens
 
-parseBrackets :: Expression -> [Token] -> (Expression, [Token])
-parseBrackets callable (BracketOpen : tokens) = parseBrackets newCallable tokensAfterCall
+parseOperationWith :: ([Token] -> (Expression, [Token])) -> [Operator] -> Expression -> [Token] -> (Expression, [Token])
+parseOperationWith parseFn operators first tokens
+    | isOperator = parseOperationWith parseFn operators (BinaryOperation operator first second) restTokens
+    | otherwise = (first, tokens)
     where
-        (arguments, _, tokensAfterCall) = parseBlock [BracketClose] [] tokens
-        newCallable = Call callable arguments
-parseBrackets callable tokens = (callable, tokens)
+        (second, restTokens) = parseFn (tail tokens)
+        (isOperator, operator) = case tokens of
+            Operator op : _ -> (op `elem` operators, op)
+            _ -> (False, undefined)
 
-parseCall :: [Token] -> (Expression, [Token])
-parseCall tokens = parseBrackets value restTokens
-    where (value, restTokens) = parseValue tokens
+parseExpressionWith :: ([Token] -> (Expression, [Token])) -> [Operator] -> [Token] -> (Expression, [Token])
+parseExpressionWith parseFn operators tokens = parseOperationWith parseFn operators first restTokens
+    where (first, restTokens) = parseFn tokens
 
-parseBinaryOperation :: Expression -> [Token] -> (Expression, [Token])
-parseBinaryOperation current (Operator operator : tokens) = parseBinaryOperation operation restTokens
-    where
-        (operand, restTokens) = parseCall tokens
-        operation = BinaryOperation operator current operand
-parseBinaryOperation current tokens = (current, tokens)
-
-parseExpression :: [Token] -> (Expression, [Token])
-parseExpression tokens = parseBinaryOperation value restTokens
-    where (value, restTokens) = parseCall tokens
+parseApplication = parseExpressionWith parseValue [Apply]
+parseExpression = parseExpressionWith parseApplication [Plus, Minus, Multiply, Divide, IntDivide, Modulo, And, Or, Xor, RaiseToThePowerOf,
+    Assign, Equal, NotEqual, GreaterThan, LowerThan, GreaterThanOrEqualTo, LowerThanOrEqualTo, Apply]
 
 parseBlock :: [Token] -> [Expression] -> [Token] -> ([Expression], Token, [Token])
 parseBlock terminators expressions tokens
