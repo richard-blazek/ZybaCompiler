50671f12c66ddd73b4ce7d8cfa05af1530d21956
Richard Blažek <blazek.risa@gmail.com>
2022-01-12 11:50:20 +0100

    Code compiled successfully

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,18 @@ module Main where
 import qualified System.IO as IO
 import qualified System.Environment as Env
 
-import Lexer (tokenize)
-import Parser (parse)
-import Semantics (analyse)
-import Codegen (generate)
-import Functions ((??), (!?))
-import Fallible (Fallible (..))
+import Loader (load)
+import Codegen (gen)
+import Functions ((!?), (??=))
+import Fallible (Fallible (..), FallibleT (..))
 
-process :: Monad m => (a -> c) -> m a -> (c -> m b) -> m b
-process transformation input output = fmap transformation input >>= output
-
-compile :: String -> String
-compile s = case fmap generate $ parse (tokenize s) >>= analyse of
-  Error err -> "Compilation failed\n" ++ err
-  Ok code -> code
-
-run :: [String] -> IO ()
-run args = process compile input output
-  where input = fmap IO.readFile (args !? 0) ?? IO.getLine
-        output = fmap IO.writeFile (args !? 1) ?? IO.putStrLn
+compile :: [String] -> IO ()
+compile (input : output : args) = do
+  result <- runFallibleT $ fmap (uncurry gen) $ load input
+  case result of
+    Ok code -> IO.writeFile output code
+    Error err -> IO.hPutStrLn IO.stderr err
+compile _ = IO.hPutStrLn IO.stderr "Input or output file missing"
 
 main :: IO ()
-main = Env.getArgs >>= run
+main = Env.getArgs >>= compile
